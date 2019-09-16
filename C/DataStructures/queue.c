2b75c1f1a98761196562694295e9543cd56df56b
Frederico Gomes da Silva <frederico.silva@radixeng.com.br>
2019-09-15 22:26:13 -0300

    update queue

--- C/DataStructures/queue.c
+++ C/DataStructures/queue.c
@@ -6,59 +6,42 @@
 #define MAX 10
 
 int queue[MAX];
-int front = -1;
-int rear = -1;
+int rear = 0;
 
 int peek()
 {
-    return queue[front];
+    return queue[0];
 }
 
 bool isEmpty()
 {
-    return front == -1;
+    return rear == 0;
 }
 
 bool isFull()
 {
-    return (front == 0 && rear == MAX - 1);
+    return (rear == MAX - 1);
 }
 
 void enqueue(int data)
 {
     if (!isFull())
     {
-        if (front == -1)
-        {
-            front = 0;
-        }
-        
-        rear = rear + 1;
         queue[rear] = data;
+        rear++;
     }
 }
-
-// void dequeue()
-// {
-//     if (!isEmpty())
-//     {
-//         front = front + 1;
-//     }
-// }
 
 void dequeue()
 {
     if (!isEmpty())
     {
-        if (front >= rear)
+        int i;
+        for (i = 0; i < rear; i++)
         {
-            front = -1;
-            rear = -1;
+            queue[i] = queue[i + 1];
         }
-        else
-        {
-            front++;
-        }
+        rear--;
     }
 }
 
@@ -71,7 +54,7 @@ void printQueue()
     else
     {
         printf("\n[");
-        for (int i = front; i <= rear; i++)
+        for (int i = 0; i < rear; i++)
         {
             printf(" %d ", queue[i]);
         }
@@ -81,13 +64,15 @@ void printQueue()
 
 int main()
 {
-    printf("insert 5 values");
+    printf("insert values");
+    dequeue();
     enqueue(3);
     enqueue(5);
     enqueue(9);
     enqueue(1);
     enqueue(12);
     enqueue(15);
+    enqueue(-4);
     printQueue();
     if (isFull())
     {
@@ -101,9 +86,11 @@ int main()
     printQueue();
     dequeue();
     enqueue(99);
-    enqueue(12);
+    enqueue(13);
     printQueue();
     printf("Element at front: %d\n", peek());
     dequeue();
+    printQueue();
+    enqueue(34);
     printQueue();
 }
