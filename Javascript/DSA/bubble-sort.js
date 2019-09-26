30f8a070eb553fc19f20afda70214a182203b06e
Frederico Gomes <gomes@Fredericos-MacBook-Pro.local>
2019-09-25 23:10:20 -0300

    creating bubble-sort.js

--- Javascript/DSA/bubble-sort.js
+++ Javascript/DSA/bubble-sort.js
@@ -1,5 +1,12 @@
 "use strict";
 
+//A ordenação por bolha (bubble sort) é um algoritmo de classificação simples. 
+//Esse algoritmo de classificação é um algoritmo baseado em comparação,
+//no qual cada par de elementos adjacentes é comparado 
+//e os elementos são trocados se não estiverem em ordem. 
+//Esse algoritmo não é adequado para grandes conjuntos de dados,
+//pois sua complexidade média e no pior caso são de Ο(n2) em que n é o número de itens.
+
 function bubbleSort(array) {
     for (let i = 0; i < array.length; i++) {
         for (let j = 0; j < array.length - 1; j++) {
