ace522cb5da08185f88bda06c089d0a94d50a137
Frederico Gomes <gomes@Fredericos-MacBook-Pro.local>
2020-02-15 13:35:12 -0300

    upating dictionary in js

--- Javascript/dictionary.js
+++ Javascript/dictionary.js
@@ -1,58 +1,35 @@
 
 function Dictionary() {
-    let items = {};
-    this.has = function (key) {
-        return key in items;
-    }
-    this.set = function (key, value) {
+    let items = new Array();
+
+    this.add = function(key, value) {
         items[key] = value;
     }
-    this.delete = function (key) {
-        if (this.has(key)) {
-            delete items[key];
-            return true;
-        }
-        return false;
+    this.find = function(key) {
+        return items[key];
     }
-    this.get = function (key) {
-        return this.has(key) ? items[key] : undefined;
+    this.remove = function(key) {
+        delete items[key];
     }
-    this.values = function () {
-        let values = [];
-        for (let k in items) {
-            if (this.has(k)) {
-                values.push(items[k]);
-            }
-        }
-        return values;
+    this.display = function() {
+        Object.keys(items).forEach(k => {
+            console.log(k + "=>" + items[k]);
+        });
     }
-    this.keys = function () {
-        return Object.keys(items);
+    this.clear = function() {
+        items = new Array();
     }
-    this.clear = function () {
-        items = {};
-    }
-    this.size = function () {
-        return Object.keys(items).length;
-    }
-    this.getItems = function () {
-        return items;
-    }
+
 }
 
 let dictionary = new Dictionary();
-dictionary.set("han", "hansolo@email.com");
-dictionary.set("luke", "luke@email.com");
-dictionary.set("leia", "leia@email.com");
+dictionary.add("han", "hansolo@email.com");
+dictionary.add("luke", "luke@email.com");
+dictionary.add("leia", "leia@email.com");
 
-console.log(dictionary.has("leia"))
-console.log(dictionary.has("yoda"))
-console.log(dictionary.size())
-console.log(dictionary.keys())
-console.log(dictionary.values())
-console.log(dictionary.get('luke'))
-console.log(dictionary.delete('luke'))
-console.log(dictionary.size())
-console.log(dictionary.keys())
-console.log(dictionary.values())
-console.log(dictionary.getItems())
+console.log(dictionary.find("leia"))
+console.log(dictionary.find("yoda"))
+console.log(dictionary.display())
+console.log(dictionary.find('luke'))
+console.log(dictionary.remove('luke'))
+console.log(dictionary.display())
