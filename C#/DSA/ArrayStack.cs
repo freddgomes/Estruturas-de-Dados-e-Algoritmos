6754e4071a228538a1745fd6f2de66fa466e4689
Frederico Gomes <fgomes@Fredericos-MacBook-Pro.local>
2020-03-12 21:31:19 -0300

    updating repository - C#

---
+++ C#/DSA/ArrayStack.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Algorithms
+{
+    public class ArrayStack<T>
+    {
+        private T[] array = new T[10];
+        private int count = 0;
+
+        public ArrayStack()
+        {
+        }
+        public void Push(T item)
+        {
+            array[count] = item;
+            count++;
+        }
+
+        public T Pop()
+        {
+            var item = array[--count];
+            return item;
+        }
+
+        public override string ToString()
+        {
+            var str = "";
+            array.ToList().ForEach(x => str = str + x.ToString() + " ");
+            return str;
+        }
+    }
+}
